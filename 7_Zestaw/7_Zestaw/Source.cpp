9325c74d0bf955c46228e33f458487f2b3455a04
pawelsa <pawel.sapek97@gmail.com>
2016-11-27 16:45:37 +0100

    Dodanie tesci

--- 7_Zestaw/7_Zestaw/Source.cpp
+++ 7_Zestaw/7_Zestaw/Source.cpp
@@ -2,6 +2,16 @@
 
 int main(){
 
+// 1	//Define a structure storing complex numbers.Define four basic operations(+, -, *, / ) as functions accepting two complex numbers and returnig a complex number as a result.
+		//Additionally, define a function accepting two real numbers and composing one complex number from them.Check, how all these functions work.
 
+
+
+// 2	//Define a structure containing coordinates of a vector in a 3 - dimensional space.Define an array of 5 such structures, 
+		//fill them with data.Write code to find the longest vector.
+
+// 3	//Write a function creating a string composed of n asterisks, where n is the parameter of this function.
+		//The result should be a pointer to a static array defined in this function.For example, the code printf("%s", aster(10)) should print **********.
+	
 	return 0;
 }
